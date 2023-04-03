7a6ba253373ad2550dc828ccbb889b86697e9a10
OneTooMany <santosismael.115@gmail.com>
2023-04-03 12:32:06 -0700

    Create basic obj constructor for book objects

---
+++ library.js
@@ -0,0 +1,13 @@
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author =  author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return title + author + pages + read;
+    }
+}
+
+const theHobbit = new book('The Hobbit ', 'by J.R.R. Tolkien, ', '295 pages, ', 'not read yet ');
+console.log(theHobbit.info());
+console.log(5);
