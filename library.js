4dd75437740998e069022dbda4fd4020ee372435
OneTooMany <santosismael.115@gmail.com>
2023-04-03 15:10:58 -0700

    Create function that takes user input and stores in array
    The function AddBookToLibrary prompts user for title
    to book. It then pushes that myLibrary array.

--- library.js
+++ library.js
@@ -1,13 +1,28 @@
-function book(title, author, pages, read) {
-    this.title = title;
-    this.author =  author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return title + author + pages + read;
-    }
+let myLibrary = ["The Toll", "A New Hope", "The Adventurs of.."];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return title + author + pages + read;
+  };
 }
 
-const theHobbit = new book('The Hobbit ', 'by J.R.R. Tolkien, ', '295 pages, ', 'not read yet ');
+function addBookToLibrary() {
+  let title = prompt("What is your books title?");
+  myLibrary.push(title);
+  console.table(myLibrary);
+}
+
+addBookToLibrary();
+
+/* const theHobbit = new Book(
+  "The Hobbit ",
+  "by J.R.R. Tolkien, ",
+  "295 pages, ",
+  "not read yet "
+);
 console.log(theHobbit.info());
-console.log(5);
+console.log(5); */
